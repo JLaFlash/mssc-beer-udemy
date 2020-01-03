84b2ba39c3ae3028d605c95fe7e9a4e34762a286
Jody LaFlash <jodylaflash@outlook.com>
2020-01-03 11:29:18 -0600

    added Beer repository

---
+++ mssc-beer-service/src/main/java/net/laflash/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package net.laflash.msscbeerservice.repositories;
+
+import net.laflash.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
